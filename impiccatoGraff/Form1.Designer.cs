75ae6362a5bf75e9131dd6cd5b0de1958a680e9a
contidavide-sudo <conti.davide@franchettisalviani.net>
2026-05-12 23:26:49 +0200

    Inizio generazione parola casuale e caselle delle lettere

--- impiccatoGraff/Form1.Designer.cs
+++ impiccatoGraff/Form1.Designer.cs
@@ -32,55 +32,79 @@
             textInsert = new TextBox();
             labelInsert = new Label();
             buttonInsert = new Button();
+            label1 = new Label();
+            GenPar = new Button();
             SuspendLayout();
             // 
             // labelTitolo
             // 
             labelTitolo.AutoSize = true;
-            labelTitolo.Font = new Font("Swis721 BdCnOul BT", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelTitolo.Font = new Font("Microsoft Sans Serif", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
             labelTitolo.ForeColor = SystemColors.MenuHighlight;
-            labelTitolo.Location = new Point(225, 32);
+            labelTitolo.Location = new Point(177, 56);
             labelTitolo.Name = "labelTitolo";
-            labelTitolo.Size = new Size(493, 58);
+            labelTitolo.Size = new Size(757, 69);
             labelTitolo.TabIndex = 0;
             labelTitolo.Text = "GIOCO DELL' IMPICCATO";
             labelTitolo.TextAlign = ContentAlignment.TopCenter;
-            labelTitolo.Click += label1_Click;
             // 
             // textInsert
             // 
-            textInsert.Location = new Point(607, 275);
+            textInsert.Location = new Point(694, 413);
+            textInsert.Margin = new Padding(3, 4, 3, 4);
             textInsert.Name = "textInsert";
-            textInsert.Size = new Size(107, 23);
+            textInsert.Size = new Size(122, 27);
             textInsert.TabIndex = 1;
             // 
             // labelInsert
             // 
             labelInsert.AutoSize = true;
-            labelInsert.Location = new Point(607, 246);
+            labelInsert.Location = new Point(694, 374);
             labelInsert.Name = "labelInsert";
-            labelInsert.Size = new Size(111, 15);
+            labelInsert.Size = new Size(139, 20);
             labelInsert.TabIndex = 2;
             labelInsert.Text = "Inserisci una lettera:";
             // 
             // buttonInsert
             // 
-            buttonInsert.Location = new Point(735, 275);
+            buttonInsert.Location = new Point(840, 413);
+            buttonInsert.Margin = new Padding(3, 4, 3, 4);
             buttonInsert.Name = "buttonInsert";
-            buttonInsert.Size = new Size(82, 23);
+            buttonInsert.Size = new Size(94, 31);
             buttonInsert.TabIndex = 3;
             buttonInsert.Text = "Inserisci";
             buttonInsert.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(721, 216);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 20);
+            label1.TabIndex = 4;
+            // 
+            // GenPar
+            // 
+            GenPar.Location = new Point(694, 216);
+            GenPar.Name = "GenPar";
+            GenPar.Size = new Size(206, 46);
+            GenPar.TabIndex = 5;
+            GenPar.Text = "Genera parola";
+            GenPar.UseVisualStyleBackColor = true;
+            GenPar.Click += GenPar_Click;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(907, 519);
+            ClientSize = new Size(1096, 692);
+            Controls.Add(GenPar);
+            Controls.Add(label1);
             Controls.Add(buttonInsert);
             Controls.Add(labelInsert);
             Controls.Add(textInsert);
             Controls.Add(labelTitolo);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -93,5 +117,7 @@
         private TextBox textInsert;
         private Label labelInsert;
         private Button buttonInsert;
+        private Label label1;
+        private Button GenPar;
     }
 }
--- impiccatoGraff/Form1.cs
+++ impiccatoGraff/Form1.cs
@@ -1,14 +1,56 @@
+using System.Windows.Forms;
+using System.IO;
+using System.ComponentModel;
+using System.Reflection.Emit;
+
 namespace impiccatoGraff
 {
     public partial class Form1 : Form
     {
+        List<string> parole = new List<string>();
+        Random rnd = new Random();
+        string par;
+        Label[] labels;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void GenPar_Click(object sender, EventArgs e)
         {
+            if (parole.Count == 0)
+            {
+                using (StreamReader sr = new StreamReader("paroleImp.csv"))
+                {
+                    sr.ReadLine();
+                    string riga = sr.ReadLine();
+
+                    while (riga != null)
+                    {
+                        parole.Add(riga);
+                        riga = sr.ReadLine();
+                    }
+                }
+            }
+            else
+            {
+                par = parole[rnd.Next(0, parole.Count)];
+
+                labels = new Label[par.Length];
+
+                for (int i = 0; i < par.Length; i++)
+                {
+                    labels[i] = new Label();
+                    labels[i].Text = "_";
+                    labels[i].AutoSize = true;
+                    labels[i].Font = new Font("Arial", 20);
+                    labels[i].Location = new Point(20 + i * 40, 50);
+
+                    this.Controls.Add(labels[i]);
+                }
+            }
+        }
+
 
         }
     }
